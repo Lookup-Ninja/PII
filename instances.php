51f58fcdfad3aae2106c021f734b43c42209b4f8
Jacob Sammon <jacob@eml.pm>
2021-09-01 21:20:45 +0100

    Create instances.php

---
+++ instances.php
@@ -0,0 +1,8 @@
+<?php
+// https://github.com/Lookup-Ninja/PII/blob/main/CONTRIBUTE.md
+$instances = array(
+  
+    array('instance.lookup.ninja', '1.0', 'FI', 'DE-HETZNER', TRUE, TRUE, TRUE),
+  
+);
+?>
